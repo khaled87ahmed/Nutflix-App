b75f3270b255b0ecbb7b442add1ba9ab8ad65096
khaled87ahmed <89733442+khaled87ahmed@users.noreply.github.com>
2023-09-14 18:23:40 +0300

    adding dist

---
+++ dist/assets/Pagenotfound-4a9681b1.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-7eb21447.js";const t="/Nutflix-App/assets/404-75568d11.png";function n(){return e("div",{className:"w-100 h-100 d-flex justify-content-center align-items-center",children:e("img",{src:t,className:"w-50 ",alt:""})})}export{n as default};
--- dist/assets/Pagenotfound-b008e631.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-6d175f7e.js";const t="/assets/404-75568d11.png";function n(){return e("div",{className:"w-100 h-100 d-flex justify-content-center align-items-center",children:e("img",{src:t,className:"w-50 ",alt:""})})}export{n as default};
